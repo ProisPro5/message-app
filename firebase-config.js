57cfa42463259254e5ef39133564af66ebaae4a1
ProisPro5 <164252434+ProisPro5@users.noreply.github.com>
2024-08-29 21:44:14 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBIwzX0EoWK54b9wvC_0BEAGiQZoI2yZjU",
   authDomain: "messaging-app-8bc67.firebaseapp.com",
+  databaseURL: "https://messaging-app-8bc67-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "messaging-app-8bc67",
   storageBucket: "messaging-app-8bc67.appspot.com",
   messagingSenderId: "393710752094",
@@ -14,7 +14,8 @@ const firebaseConfig = {
   measurementId: "G-E4SSQ97H5W"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const firestore = getFirestore(app);
+
+export { app, auth, firestore };
